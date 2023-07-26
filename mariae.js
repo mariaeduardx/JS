86683671ac2774a86847c3819792eecf2a1a3b3f
Maria Eduarda <maria.eduarda.alves.silva@hotmail.com>
2023-07-26 14:32:00 -0300

    first commit

---
+++ mariae.js
@@ -0,0 +1,43 @@
+console.log("maria diva")
+
+console.log("17")
+
+//total de semanas que eu programei em casa
+console.log("3 semanas em casa")
+
+/*meu nome é Maria Eduarda, tenho 17 anos, e estou no curso de desenvolvimento
+de sistemas no senai à 6 meses*/
+
+let number = 17;
+
+let name = "Maria Eduarda";
+
+let object = "lápis";
+
+let nulo = null;
+
+let booleano = true;
+
+console.log(17+3.5)
+
+console.log(2023 - 1969)
+
+console.log(65/240)
+
+console.log(0.2708*100)
+
+console.log("hello" + "world")
+
+console.log("hello world")
+
+console.log('duda'.length)
+
+console.log('casa'.toUpperCase())
+
+console.log(' adoro ler ' .trim())
+
+console.log(100*Math.random())
+
+console.log(Math.ceil(43.8))
+
+console.log(2017*Number.isInteger())
